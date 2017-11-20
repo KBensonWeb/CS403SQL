9f6396bae75bab0675078faa60c1cd885c8f6dc8
KBensonWeb <KBensonWeb@gmail.com>
2017-11-20 14:19:07 -0500

    Push 2

--- app/src/main/java/com/androidbash/androidbashphpmysql/AddMovieActivity.java
+++ app/src/main/java/com/androidbash/androidbashphpmysql/AddMovieActivity.java
@@ -1,13 +1,25 @@
 package com.androidbash.androidbashphpmysql;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
+import okhttp3.FormBody;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
+
+import static com.androidbash.androidbashphpmysql.R.id.genreA;
+import static com.androidbash.androidbashphpmysql.R.id.imageA;
+import static com.androidbash.androidbashphpmysql.R.id.mTitleA;
 
 public class AddMovieActivity extends AppCompatActivity {
 
@@ -17,15 +29,26 @@ public class AddMovieActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_movie);
     }
 
-    private void addMovieToDB(String movieInfo) {
+    public void addMovieToDB(View view) {
+        EditText nameIn = (EditText) findViewById(mTitleA);
+        String name = nameIn.getText().toString();
+        EditText genreIn = (EditText) findViewById(genreA);
+        String genre = genreIn.getText().toString();
+        EditText imageIn = (EditText) findViewById(imageA);
+        String image = imageIn.getText().toString();
 
         AsyncTask<String, Void, Void> asyncTask = new AsyncTask<String, Void, Void>() {
             @Override
             protected Void doInBackground(String... movieInfo) {
-
                 OkHttpClient client = new OkHttpClient();
+                RequestBody formBody = new FormBody.Builder()
+                        .add("name", movieInfo[0])
+                        .add("image", movieInfo[1])
+                        .add("genre", movieInfo[2])
+                        .build();
                 Request request = new Request.Builder()
-                        .url("https://kbensonweb.com/movies_add.php?name=" + movieInfo[0] + "&image=" + movieInfo[1] + "&genre=" + movieInfo[2])
+                        .url("https://kbensonweb.com/movies_add.php")
+                        .post(formBody)
                         .build();
                 try {
                     client.newCall(request).execute();
@@ -34,15 +57,12 @@ public class AddMovieActivity extends AppCompatActivity {
                 }
                 return null;
             }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                MoviesAdapter adapter;
-                adapter.notifyDataSetChanged();
-            }
         };
 
-        asyncTask.execute(movieInfo);
+        asyncTask.execute(name,image,genre);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
 
     }
 }
--- app/src/main/java/com/androidbash/androidbashphpmysql/MainActivity.java
+++ app/src/main/java/com/androidbash/androidbashphpmysql/MainActivity.java
@@ -1,10 +1,13 @@
 package com.androidbash.androidbashphpmysql;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -60,6 +63,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void addMovie(View view) {
+        Intent intent = new Intent(this, AddMovieActivity.class);
+        startActivity(intent);
+    }
 
     private void getMoviesFromDB(int id) {
 
